7374a41bcddbd160f7939cdee30b98acc28a5e6c
mrmazakblu <mrmazakblu@gmail.com>
2020-10-02 22:16:07 -0400

    Attempt to move to use common trre along with device tree

--- omni_Note_9P.mk
+++ omni_Note_9P.mk
@@ -1,11 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := Note_9P
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from MTK/common_mt676_
+$(call inherit-product, device/MTK/common_mt676_/omni_common_mt676_.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := ulefone
@@ -15,12 +12,3 @@ PRODUCT_MODEL := Note_9P
 PRODUCT_NAME := omni_Note_9P
 PRODUCT_BOARD := mt6765
 
-# enable stock zip packages flash
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.treble.enabled=true \
-    persist.sys.usb.config=mtp \
-    persist.service.adb.enable=1 \
-    persist.service.debuggable=1 \
-    ro.secure=1 \
-    ro.adb.secure=0 \
-    ro.allow.mock.location=0
