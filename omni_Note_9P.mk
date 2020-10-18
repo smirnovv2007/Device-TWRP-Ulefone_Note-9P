7a8446e538d5dac6b587255f8a1eb5dda37ddb4c
mrmazakblu <mrmazakblu@gmail.com>
2020-10-18 14:03:42 -0400

    still adding stock ashmem- not booting- next time build them

--- omni_Note_9P.mk
+++ omni_Note_9P.mk
@@ -5,16 +5,16 @@ PRODUCT_RELEASE_NAME := Note_9P
 $(call inherit-product, device/MTK/common_mt676_/common_mt676_.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_BRAND := ulefone
+PRODUCT_BRAND := Ulefone
 PRODUCT_DEVICE := Note_9P
 PRODUCT_MANUFACTURER := Ulefone
-PRODUCT_MODEL := Note_9P
+PRODUCT_MODEL := Note 9P
 PRODUCT_NAME := omni_Note_9P
 PRODUCT_BOARD := mt6765
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=Note_9P \
-    PRODUCT_NAME=Note_9P \
+    TARGET_DEVICE='Note 9P' \
+    PRODUCT_NAME='Note 9P' \
     PRIVATE_BUILD_DESC="full_k62v1_64_bsp-user 10 QP1A.190711.020 mp1V91221 release-keys"
 
 BUILD_FINGERPRINT := 10/QP1A.190711.020/1592573130:user/release-keys
